c29d18a778e0b28174e8744a360f29ab0d35869e
Jordan Bhar <suv300@icloud.com>
2023-10-29 20:20:16 -0400

    Made Navbar Zinc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,8 +29,8 @@ const Navbar = ({ educationRef, projectsRef, contactRef }) => {
 
   // Apply the 'fixed-nav' class conditionally based on if the
   const navClass = isFixed
-    ? "fixed right-0 left-0 top-0 transition ease-in-out delay-150 bg-zinc-900 opacity-80 rounded-lg shadow-2xl shadow-cyan-600/50"
-    : "bg-acrylic rounded-lg shadow-2xl shadow-cyan-600/50 mt-[10px]";
+    ? "fixed right-0 left-0 top-0 transition ease-in-out delay-150 bg-zinc-900  rounded-lg shadow-2xl shadow-cyan-600/50"
+    : "bg-zinc-900  rounded-lg shadow-2xl shadow-cyan-600/50 mt-[10px]";
 
   return (
     <nav className={`${navClass} z-50`}>
@@ -79,7 +79,7 @@ const Navbar = ({ educationRef, projectsRef, contactRef }) => {
           <div
             className={`${
               toggle ? "flex" : "hidden"
-            }  p-6 absolute top-28 md:right-10 min-w-[300px] p:min-w-[250px]  bg-acrylic rounded-xl sidebar justify-end mt-[10px]`}
+            }  p-6 absolute top-28 md:right-10 min-w-[300px] p:min-w-[250px]  bg-zinc-900 rounded-xl sidebar justify-end mt-[10px]`}
           >
             <ul className={`list-none flex flex-col items-end`}>
               {navLinks.map((nav, index) => (

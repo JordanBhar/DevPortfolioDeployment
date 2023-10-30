fb2e8910411fe7654b2741a5ad924cc510117bb8
Jordan Bhar <suv300@icloud.com>
2023-10-30 18:39:02 -0400

    Fixed Responsive Design Sizing

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,21 +3,20 @@ import { socialMediaLinks } from "../constants";
 
 const Links = () => {
   return (
-    <div className="flex justify-center space-x-[45px] sm:space-x-[75px] lg:space-x-[150px]">
+    <div className="flex justify-center space-x-[45px] sm:space-x-[75px] lg:space-x-[150px] mt-[30px]">
       {socialMediaLinks.map((socialMedia) => (
         <div className="flex-row">
           <a
             href={`${socialMedia.link}`}
-            className="bg-teal-400 text-white w-[75px] h-[75px] rounded-full flex items-center justify-center transition-transform transform ease-in-out duration-300 hover:scale-125"
+            className="bg-teal-400 text-white w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] md:w-[55px] md:h-[55px] lg:w-[75px] lg:h-[75px] rounded-full flex items-center justify-center transition-transform transform ease-in-out duration-300 hover:scale-125"
           >
             <img
               src={`${socialMedia.icon}`}
-              width="35px"
-              height="35px"
-              className="text-white"
+              
+              className="text-white w-[20px] h-[20px] sm:w-[25px] sm:h-[25px] md:w-[35px] md:h-[35px] lg:w-[45px] lg:h-[45px]"
             ></img>
           </a>
-          <p className="text-center mt-3">{socialMedia.name}</p>
+          <p className="text-center mt-3 text-sm md:text-md">{socialMedia.name}</p>
         </div>
       ))}
       {/* Add more icons as needed */}
